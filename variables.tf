d92d2c6f16da8db8031d34e9d94e85fe6e7e0103
Max Brain <max.brain@ksoc.com>
2023-08-17 16:27:15 +0300

    Fix: Moves hardcoded strings within the module into variables
    Signed-off-by: Max Brain <max.brain@ksoc.com>

--- variables.tf
+++ variables.tf
@@ -12,3 +12,15 @@ variable "ksoc_secret_key" {
   type        = string
   description = "Ksoc API secret"
 }
+
+variable "ksoc_assumed_role_arn" {
+  type        = string
+  description = "KSOC Role that will be allowed to assume"
+  default     = "arn:aws:iam::955322216602:role/ksoc-connector"
+}
+
+variable "ksoc_api_url" {
+  type        = string
+  description = "KSOC API to use when registering an account"
+  default     = "https://api.ksoc.com"
+}
