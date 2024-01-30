3cbeb29355cf5e8b580fff247b14e8cfd3221fa1
Max Brain <max.brain@ksoc.com>
2024-01-30 17:50:27 +0800

    feat: Use specific IAM actions instead of full ReadOnly access
    Signed-off-by: Max Brain <max.brain@ksoc.com>

--- main.tf
+++ main.tf
@@ -21,12 +21,6 @@ resource "aws_iam_role" "this" {
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
-# Take the AWS readonly role
-resource "aws_iam_role_policy_attachment" "readonly" {
-  role       = aws_iam_role.this.name
-  policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
-}
-
 # Instance profile
 resource "aws_iam_instance_profile" "this" {
   name = aws_iam_role.this.name
